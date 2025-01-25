9eb89ac6581564dcf3bc6d379711930c3975341a
Vamsi Krishna <63628662+VamsiKrishna082@users.noreply.github.com>
2025-01-25 23:42:04 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// CORS configuration
+const corsOptions = {
+  origin: 'https://ioinfo.shop', // Allow only your domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+};
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
+// Enable CORS with the specified options
+app.use(cors(corsOptions));
+
 // API endpoint
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello, World!!! New version' });
+  res.json({ message: 'New Hello, World! Connected to Backend successfully' });
 });
 
 // Start the server
